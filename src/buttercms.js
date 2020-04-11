533fd73d1249cba4053f945ed1f02140621a69bf
Andrew Kim <akinhwan@gmail.com>
2020-04-11 11:49:44 -0400

    none github pages, netlify now!

---
+++ src/buttercms.js
@@ -0,0 +1,2 @@
+import Butter from "buttercms";
+const butter = Butter("a639f10fa448783a957c43de356ef6761a8b7740");
--- src/router.js
+++ src/router.js
@@ -1,25 +1,37 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Main from './components/Main.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Main from "./components/Main.vue";
+import BlogHome from "@/components/BlogHome";
+import BlogPost from "@/components/BlogPost";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'main',
+      path: "/",
+      name: "main",
       component: Main
     },
     {
-      path: '/main2',
-      name: 'main2',
+      path: "/main2",
+      name: "main2",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: function () { 
-        return import(/* webpackChunkName: "about" */ './components/Main2.vue')
+      component: function() {
+        return import(/* webpackChunkName: "about" */ "./components/Main2.vue");
       }
+    },
+    {
+      path: "/blog/",
+      name: "blog-home",
+      component: BlogHome
+    },
+    {
+      path: "/blog/:slug",
+      name: "blog-post",
+      component: BlogPost
     }
   ]
-})
+});
